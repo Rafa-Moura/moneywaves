058b62d4d6e535044c203570ecfdd54b0f7254d6
rafael moura <rafaelfelipe.peixoto@gmail.com>
2023-10-29 14:36:51 -0300

    Iniciando o projeto

---
+++ moneywaves-api/src/main/java/br/com/moneywavesapi/MoneyWavesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.moneywavesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWavesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyWavesApiApplication.class, args);
+	}
+
+}
---
+++ moneywaves-api/src/test/java/br/com/moneywavesapi/MoneyWavesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moneywavesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWavesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
